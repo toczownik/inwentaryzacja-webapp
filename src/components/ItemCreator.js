287193d10ac547c359bd818e26172c3bf1b6123a
Jan Szataniak <jan.szataniak@gmail.com>
2023-01-02 14:59:35 +0100

    fixed adding items

--- src/components/ItemCreator.js
+++ src/components/ItemCreator.js
@@ -2,12 +2,37 @@ import {useNavigate} from "react-router-dom";
 import {Button, Container, Form} from "react-bootstrap";
 import {useState} from "react";
 import ItemService from "../services/item.service";
+import Select from "react-select";
+import {SingleValue} from "react-select/animated";
 
 function ItemCreator() {
     let navigate = useNavigate();
     const [id, setId] = useState(0);
     const [element, setElement] = useState({});
     const [message, setMessage] = useState("");
+    const [name, setName] = useState("");
+    const [purchaseDate, setPurchaseDate] = useState(new Date());
+    const [faxNumber, setFaxNumber] = useState(0);
+    const [price, setPrice] = useState(0);
+    const [depreciation, setDepreciation] = useState(0);
+    const [currencyValue, setCurrencyValue] = useState(0);
+    const [description, setDescription] = useState("");
+    const [loc, setLoc] = useState("");
+    const [category, setCategory] = useState("");
+
+    const categories = [
+        {label: "grunty", value: "LAND"},
+        {label: "budynki i lokale oraz spółdzielcze prawo do lokalu użytkowego i spółdzielcze własnościowe prawo do lokalu mieszkalnego", value: "BUILDINGS_DWELLING"},
+        {label: "obiekty inżynierii lądowej i wodnej", value: "CIVIL_ENGINEER_STRUCTURE"},
+        {label: "kotły i maszyny energetyczne", value: "BOILER_POWER_MACHINERY"},
+        {label: "maszyny, urządzenia i aparaty ogólnego zastosowania", value: "SPECIALIZED_MACHINERY_EQUIPMENT_GENERAL_USE"},
+        {label: "maszyny, urządzenia i aparaty specjalistyczne", value: "SPECIALIZED_MACHINERY_EQUIPMENT_APPARATUS"},
+        {label: "urządzenia technicznie", value: "TECHNICAL_EQUIPMENT"},
+        {label: "środki transportu", value: "TRANSPORT"},
+        {label: "narzędzia, przyrządy, ruchomości i wyposażenie, gdzie indziej niesklasyfikowane", value: "ANOTHER"},
+        {label: "inwentarz żywy", value: "LIVESTOCK"}
+    ]
+
     const onFormSubmit = e => {
         e.preventDefault();
         ItemService.getItem(id).then(
@@ -23,9 +48,29 @@ function ItemCreator() {
     };
 
     const toLogs = e => {
+        e.preventDefault();
         navigate("/logs");
         window.location.reload();
     };
+
+    const addItem = e => {
+        e.preventDefault();
+        ItemService.addItem(name, purchaseDate, faxNumber, price, depreciation, currencyValue, description, loc,
+            category).then(
+            (response) => {
+                setElement(response.data);
+                setMessage(response.data.message);
+            },
+            (error) => {
+                const resMessage = (error.response && error.response.data) || error.message || error.toString();
+                setMessage(resMessage);
+            }
+        )
+    }
+
+    const changeCategory = e => {
+        setCategory(e.value);
+    }
 
     return (
         <Container>
@@ -41,7 +86,48 @@ function ItemCreator() {
             </Form>
             {element.name}
             <br/>
-            {message}
+            <Form onSubmit={addItem}>
+                <Form.Group controlId="text">
+                    <Form.Label>Nazwa</Form.Label>
+                    <Form.Control type="text" placeholder="name" onChange={e => {setName(e.target.value);}}/>
+                </Form.Group>
+                <Form.Group controlId="date">
+                    <Form.Label>Data zakupu</Form.Label>
+                    <Form.Control type="date" placeholder="purchase date" onChange={e => {setPurchaseDate(e.target.value);}}/>
+                </Form.Group>
+                <Form.Group controlId="fax">
+                    <Form.Label>Faks</Form.Label>
+                    <Form.Control type="text" placeholder="faks" onChange={e => {setFaxNumber(e.target.value);}}/>
+                </Form.Group>
+                <Form.Group controlId="number">
+                    <Form.Label>Cena zakupu</Form.Label>
+                    <Form.Control type="number" placeholder="cena" onChange={e => {setPrice(e.target.value);}}/>
+                </Form.Group>
+                <Form.Group controlId="number">
+                    <Form.Label>Deprecjacja</Form.Label>
+                    <Form.Control type="number" placeholder="deprecjacja" onChange={e => {setDepreciation(e.target.value);}}/>
+                </Form.Group>
+                <Form.Group controlId="number">
+                    <Form.Label>Wartość waluty</Form.Label>
+                    <Form.Control type="number" placeholder="wartość waluty" onChange={e => {setCurrencyValue(e.target.value);}}/>
+                </Form.Group>
+                <Form.Group controlId="text">
+                    <Form.Label>Opis</Form.Label>
+                    <Form.Control type="text" placeholder="opis" onChange={e => {setDescription(e.target.value);}}/>
+                </Form.Group>
+                <Form.Group controlId="text">
+                    <Form.Label>Lokalizacja</Form.Label>
+                    <Form.Control type="text" placeholder="lokalizacja" onChange={e => {setLoc(e.target.value);}}/>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Kategoria</Form.Label>
+                    <Select options={categories} components={SingleValue} onChange={changeCategory}/>
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    DODAJ
+                </Button>
+                {message}
+            </Form>
         </Container>
     );
 }
--- src/services/item.service.js
+++ src/services/item.service.js
@@ -1,26 +1,25 @@
-import axios, {AxiosRequestConfig} from "axios";
+import axios from "axios";
 
 const API_URL = "http://localhost:8080/api/v1/item/";
 
-const corsHeaders: AxiosRequestConfig = {
-    headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
-    }
-};
-
-const addItem = (name, purchaseDate, faxNumber, purchasePrice, amountOfAnnualDepreciation, currencyValue, description, location) => {
+const addItem = (name, date, faxNumber, purchasePrice, amountOfAnnualDepreciation, currencyValue, description,
+                 location, classification) => {
+    let purchaseDate = new Date(date).toISOString();
     return axios.post(API_URL, {
         name,
+        purchaseDate,
         faxNumber,
         purchasePrice,
         amountOfAnnualDepreciation,
         currencyValue,
         description,
-        location
+        location,
+        classification
     }, {
         headers: {
-            Authorization: `Bearer${localStorage.getItem("token")}`
+            Authorization: `Bearer${localStorage.getItem("token")}`,
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
         }
     });
 }
